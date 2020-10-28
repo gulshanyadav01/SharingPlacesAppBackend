a0ec59b940ad67d22cf861941f00c922493b9a76
gulshanyadav01 <gulshany01@gmail.com>
2020-10-29 05:26:42 +0530

    yet to come

--- src/places/pages/NewPlaces.jsx
+++ src/places/pages/NewPlaces.jsx
@@ -1,21 +1,56 @@
-import React from "react"
+import React, { useCallback, useReducer } from "react"
 // import { Redirect } from "react-router-dom"
 import Input from "../../shared/components/FormElements/Input"
-import { VALIDATOREQUIRE } from '../../shared/util/validators';
+import { VALIDATOR_MINLENGTH, VALIDATOR_REQUIRE } from '../../shared/util/Validators';
 import "./NewPlaces.css"
 
+const formReducer  = (state, action) => { 
+
+}
+
 const NewPlaces = () => {
+        useReducer(formReducer, {
+            inputs: {
+                title: { 
+                    value: "",
+                    isValid: false
+                },
+                description: {
+                    value: "",
+                    isValid: false
+                }
+            },
+            isValid: false
+        });
+
+
+
+        const titleInputHandler = useCallback((id, value, isValid) => { 
+
+        },[]);
+        const descriptionInputHandler = useCallback((id, value, isValid) => { 
+
+        },[]);
     return(
        <div>
            <form className = "place-form">
                 <Input
-                 type ="text"
-                  label = "Title" 
-                  element = "input"
-                errorText= "Please enter a valid tititle"
-                validators = {[VALIDATOREQUIRE()]}
+                id= "title"
+                type ="text"
+                label = "Title" 
+                element = "input"
+                validators = {[VALIDATOR_REQUIRE()]}
+                errorText= "Please enter a valid title"
+                onInput = {titleInputHandler}
                    />
-                <Input type ="text" label = "Name" element = "input"/>
+                <Input 
+                id = "description"
+                label = "description" 
+                element = "textarea"
+                validators = {[VALIDATOR_MINLENGTH(5)]}
+                errorText= "Please enter a valid description"
+                onInput = {descriptionInputHandler}
+                />
            </form>
        </div>
     )
